c2a971a1a434f71e56ae241805b22e501546785e
POWERTRICEPS <imnotdacoco05@gmail.com>
2025-06-28 00:53:31 -0700

    Contxt API

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,5 @@
+const Chat = () => {
+    return <div className="text-white">Baka123</div>;
+}
+ 
+export default Chat;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap"
+
+const Login = () => {
+  return (
+    <>
+      <Form>
+        <Row style={{height:"100vh", justifyContent:"center", paddingTop:"10%"}}>
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2 className="text-white">Login</h2>
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+              <Alert variant="danger"><p>An error has occured</p></Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
+}
+
+export default Login;
